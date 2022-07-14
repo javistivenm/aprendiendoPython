1e0e07a0d6f0c3aa5c0a7999ba6814fe68bb21f0
Javier <javistivenmurillo@gmail.com>
2022-07-14 07:21:10 -0500

    Se han realizado varios ejercicios

---
+++ strings.py
@@ -0,0 +1,26 @@
+mensaje = "Hola"
+mensaje += " "
+mensaje += "Javier"
+print(mensaje)
+
+print("Concatenación: ")
+mensaje = "Hola"
+espacio = " "
+nombre = "Javier"
+print(mensaje + espacio + nombre)
+
+numero_uno = 4
+numero_dos = 6
+resultado = numero_uno + numero_dos
+resultado = str(resultado)
+print("El resultado de la suma es: " + resultado)
+
+print("Busqueda: ")
+mensaje = "Hola Javier"
+buscar_subcadena = mensaje.find("Javier")
+print(buscar_subcadena)
+
+print("Comparación: ")
+mensaje_uno = "Hola"
+mensaje_dos = "Hol"
+print (mensaje_uno == mensaje_dos)
